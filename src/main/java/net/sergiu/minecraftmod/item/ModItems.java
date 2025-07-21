2ea101c1ae4eb91a55d30dbc67ecb72e66453622
Sergiu Ungureanu <sergiu.ungureanu2005@gmail.com>
2025-07-21 23:42:25 +0300

    Test

---
+++ src/main/java/net/sergiu/minecraftmod/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.sergiu.minecraftmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.event.RegisterColorHandlersEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.sergiu.minecraftmod.TestMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MOD_ID);
+
+    public static final RegistryObject<Item> ALEXANDRITE = ITEMS.register("alexandrite", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> ZARATHUSTRA = ITEMS.register("zarathustra", () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
